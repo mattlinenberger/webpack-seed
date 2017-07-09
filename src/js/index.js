0d357840f673af0f403d95756101811c25ca7ac4
Scott Linenberger <scott.linenberger@gmail.com>
2017-07-09 14:49:48 -0500

    import vendor style: Pure

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,11 @@
 import AppRouter from './AppRouter';
 
-/* import styles */
+
+/* import vendor styles */
+require('../../node_modules/pure-css/lib/base.css');
+require('../../node_modules/pure-css/lib/grids.css');
+
+/* import app styles */
 require('../styles/index.scss');
 
 /* create a new app router */
