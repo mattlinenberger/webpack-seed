73a75e444b638adac37dac1275cf2c363ccb289f
Scott Linenberger <=>
2017-07-27 18:13:15 -0500

    added logging params to app config

--- src/js/app.config.js
+++ src/js/app.config.js
@@ -1,5 +1,15 @@
+
+/* add loggers here that have their own log-level */
+
+const loggers = new Map();
+
+/* logger name and the custom level */
+loggers.set('SampleView', 5);
+
 export default {
   LOGGING: {
     LEVEL: 5,
+    CUSTOM_LOGGERS: loggers,
+    CUSTOM_LOGGERS_ENABLED: false,
   },
 };
