4d5e242a2f0268059b1559ffccb73c5304313304
Scott Linenberger <=>
2017-07-27 23:49:25 -0500

    add todo functions to sample view

--- src/js/views/sampleView2/SampleView.js
+++ src/js/views/sampleView2/SampleView.js
@@ -1,40 +1,59 @@
 import LoggableView from '../../modules/LoggableView';
+import ToDo from '../todo/ToDo.view';
 
 const templateMain = require('./SampleView2.handlebars');
 
+/* bring in the stylesheet */
+require('./SampleView.scss');
+
 const SampleView = LoggableView.extend({
+  /* bind events */
+  events: {
+    'click .add': 'addToDoItem',
+  },
+  /* render view */
   render() {
     this.logger.log('RENDERING!');
 
-    const items =
-      this.model
-        .map(current => current.attributes);
+    const html = templateMain();
 
-    const data = { items };
-
-    console.log('data %O', data);
-
-    const html = templateMain(data);
-
+    /* render the view template */
     this.$el.empty();
     this.$el.append(html);
+
+    /* loop through the items and create a view for each ToDo */
+    this.model
+      .forEach((currentItem) => {
+        /* create a new to-do */
+        const toDo = new ToDo({ //eslint-disable-line
+          loggerName: 'ToDo',
+          model: currentItem,
+          ctlr: this.ctlr,
+        });
+
+        /* append the item */
+        this.$('.item_list').append(toDo.el);
+      });
   },
-  update() {
-    this.logger.log('UPDATING!');
-    this.render();
+  onAdd(model) {
+    this.logger.log('ToDo added! %O', model);
+    /* construct a new ToDo view */
+    const toDo = new ToDo({
+      loggerName: 'ToDo',
+      model,
+      ctlr: this.ctlr,
+    });
+
+    this.$('.item_list').append(toDo.el);
   },
-  /* bind events */
-  events: {
-    'click button': 'handleClick',
-    'click .remove': 'removeItem',
-  },
-  removeItem(e) {
-    console.log(e);
-    const itemId = e.currentTarget.id;
-    this.ctlr.removeItem(itemId);
-  },
-  handleClick() {
-    this.ctlr.sayHello();
+  addToDoItem() {
+    const textBox = this.$('input')[0];
+    const itemText = textBox.value;
+
+    this.ctlr.addToDoItem(itemText);
+
+    /* clear the input box text */
+    textBox.value = '';
   },
 });
 
